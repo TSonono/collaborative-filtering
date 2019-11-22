d42d6e1ae49eec9c956cd1417b94052c18851e43
TSonono <tofiksonono@msn.com>
2019-11-22 16:15:31 +0100

    Added more unit tests
    The unit tests now result in 100% coverage. We need a more complex database test to confirm more functionality. We also need to differ the flags for the cold start problem in the unit tests.

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const recommend = require('../lib/cf_api.js');
+const math = require('mathjs');
 
 describe('D1 tests"', function() {
     console.log("Simple rating database (D1):")
@@ -10,23 +11,36 @@ describe('D1 tests"', function() {
     console.log("   U1  [1  0  1]");
     console.log("   U2  [1  0  0]");
 
+    describe('Create the co-occurrence matrix', function() {
+        it('The ratings matrix is a number 5 instead of an array', function() {
+            assert.throws(() => recommend.coMatrix(5), TypeError)
+        });
+        it('The ratings matrix has incorrect dimensions', function() {
+            assert.throws(() => recommend.coMatrix([[1, 1, 1], [1, 0, 1], [1, 0]]), RangeError)
+        });
+        it('The ratings matrix consists of erroneous elements', function() {
+            assert.throws(() => recommend.coMatrix([[1, 1, 1], [1, 0, 'hello'], [1 ,'world', 0]]), TypeError)
+        });
+    });
+
     const ratings = [
         [1, 1, 1],
         [1, 0, 1],
         [1, 0, 0],
       ];
     const coOccurrenceMatrix = recommend.coMatrix(ratings)
-    describe('#Recommendations', function() {
+
+    describe('Get recommendations', function() {
         it('Recommendations for U1 should be [I1]', function() {
-            recommendations =recommend.getRecommendations(ratings, coOccurrenceMatrix, 1);
+            recommendations = recommend.getRecommendations(ratings, coOccurrenceMatrix, 1);
             assert.deepEqual(recommendations, [1]);
         });
         it('Recommendations for U2 should be [I2, I1]', function() {
-        recommendations =recommend.getRecommendations(ratings, coOccurrenceMatrix, 2);
+        recommendations = recommend.getRecommendations(ratings, coOccurrenceMatrix, 2);
         assert.deepEqual(recommendations, [2, 1]);
         });
         it('Recommendations for U0 should be []', function() {
-            recommendations =recommend.getRecommendations(ratings, coOccurrenceMatrix, 0);
+            recommendations = recommend.getRecommendations(ratings, coOccurrenceMatrix, 0);
             assert.deepEqual(recommendations, []);
         });
   });
@@ -36,6 +50,25 @@ describe('D1 tests"', function() {
         });
         it('Recommendations for U47 should throw a RangeError', function() {
             assert.throws(() => recommend.getRecommendations(ratings, coOccurrenceMatrix, 47), RangeError);
+        });
+        it('A co occurrence matric which does not match the dimensions for the ratings', function() {
+            assert.throws(() => recommend.getRecommendations(ratings, math.ones(4, 7), 1), RangeError);
+        });
+        it('Creating an invalid co-occurrence matrix (three dimensional matrix)', function() {
+            assert.throws(() => recommend.getRecommendations(ratings, math.matrix([[[1, 1, 0], [1, 1, 1]], [[1, 0, 1], [0, 0, 1]]]), 1), RangeError);
+        });
+        it('Passing an array instead of a mathjs matrix as a co occurrence matrix', function() {
+            assert.throws(() => recommend.getRecommendations(ratings, [4, 7], 1), TypeError);
+        });
+        it('Passing a ratings array which is invalid', function() {
+            assert.throws(() => recommend.getRecommendations([[1, 1, 1], [1, 0, 1], [1, 0]], coOccurrenceMatrix, 1), RangeError);
+        });
+    });
+
+    describe('Tests using the cFilter function which runs the whole process and generates recommendations for a user', function() {
+        it('Run the process and generate recommendations for user 2', function() {
+            recommendations = recommend.cFilter(ratings, 2);
+            assert.deepEqual(recommendations, [2, 1]);
         });
     });
 });
